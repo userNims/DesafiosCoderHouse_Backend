000286893b621bced50c2becc151862320a2d7c1
userNims <86461767+userNims@users.noreply.github.com>
2022-05-11 18:18:52 -0300

    Desafio 07 actualizado
    Se subio la primera parte del desafio 07 con Knex-MariaDB

---
+++ 07 - Sql y Knex/create_table.js
@@ -0,0 +1,18 @@
+const { knexMysql } = require('./options/mariaDB');
+const { knexSqLite } = require('./options/sqlLite3');
+
+const createTable = async knex => {
+    await knex.schema.createTable('contenedor', table => {
+        table.increments('id').primary();
+        table.string('title');
+        table.string('autor');
+        table.string('thumbnail');
+        table.integer('price');
+        table.integer('stock');
+        table.integer('isbn');
+        table.string('description');
+    });
+}
+
+createTable(knexMysql);
+createTable(knexSqLite);
